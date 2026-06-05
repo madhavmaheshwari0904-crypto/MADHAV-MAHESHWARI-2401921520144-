ab561031a9c785744a45d7e134c6aa8a61d436d8
Madhav Maheshwari <madhavmaheshwari0904@gmail.com>
2026-06-05 16:39:39 +0530

    Day 5 Questions added.

---
+++ Week 1/Day 5/Q-1 Valid Palindrome.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        s=[i for i in s.lower() if i.isalnum()]
+        return s==s[::-1]
---
+++ Week 1/README.md
@@ -0,0 +1,3 @@
+# Madhav Maheshwari
+#2401921520144
+WEEK 1
